22d1feed18b88cd0fe969fa06e555516773aa8a1
BradD <bradleyeday@gmail.com>
2020-07-05 12:37:38 -0500

    created basic JS for daily weather

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -9,22 +9,42 @@ function formSubmitHandler(event) {
     var cityname = cityInputEl.value.trim();
 
     if (cityname) {
+        getToday(cityname);
         getWeather(cityname);
-        nameInputEl.value = "";
+        cityInputEl.value = "";
     }
     else {
         alert("Please enter a city name.")
     }
 }
 
-
-function getWeather(city) {
-    var weather = "api.openweathermap.org/data/2.5/forecast?q=" + city +  "&appid=fe8d611a841b4f7219380686ae60e97a";
+function getToday(city) {
+    var weather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=fe8d611a841b4f7219380686ae60e97a";
 
     fetch(weather).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                displayWeather(data, main)
+                displayToday(data, city);
+            })
+        }
+        else {
+            alert ("Error: " + response.status);
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to OpenWeather");
+    })
+}
+
+
+function getWeather(city) {
+    var weather = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +  "&appid=fe8d611a841b4f7219380686ae60e97a";
+
+
+    fetch(weather).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayWeather(data, city);
             });
         }
         else {
@@ -36,16 +56,42 @@ function getWeather(city) {
     })
 }
 
-function displayWeather(cityweather, searchTerm) {
+function displayToday(cityweather, searchTerm) {
     weatherContainerEl.textContent = "";
     citySearchTerm.textContent = searchTerm;
+}
+
+function displayWeather(cityweather, searchTerm) {
+    
+    
 
     if (cityweather.length === 0) {
         weatherContainerEl.textContent = "No Weather information to be found.";
         return;
     }
 
+
+
     for (var i=0; i < cityweather.lenght; i++) {
-        var 
+        var weatherDay = cityweather[i].list.dt_txt;
+
+        var weatherEl = document.createElement("div");
+        weatherEl.classList = "card";
+        
+        var weatherTemp = cityweather[i].list.main;
+
+        var weatherCondition = cityweather[i].list.weather;
+
+        var weatherObj = weatherDay + weatherTemp + weatherCondition;
+        console.log(weatherObj);
+        console.log(weatherCondition);
+        console.log(weatherTemp);
+
+
+        weatherEl.appendChild(weatherObj);
+
+        weatherContainerEl.appendChild(weatherEl);
     }
 }
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
